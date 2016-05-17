fc7a55f8e123c3da1c744f1b8b533d5ddef22daa
Shevah <shevah92@gmail.com>
2016-05-17 10:22:51 +0200

    add description to doors.py

--- doors.py
+++ doors.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+# mathematical solution: we figured a doors will be open if
+# the number of the divisors for that are odd. that's only true for square numbers
+
 for i in range(1,11):
     square= i*i
     print(square)
